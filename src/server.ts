fcf1a69da74913be2adbb98474aafd49ce19e63f
Filipe Dias <filipediascontato@gmail.com>
2021-07-21 20:43:49 -0300

    Starts to implement categories routes

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello world!" });
-});
+app.use(express.json());
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running..."));
