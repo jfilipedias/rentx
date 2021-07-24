b9ae06232013474741bc481752901c7dfa6fbfb8
Filipe <filipediascontato@gmail.com>
2021-07-24 18:23:15 -0300

    Decouples the specifications logic

--- src/modules/cars/repositories/implementations/SpecificationRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -5,13 +5,22 @@ import {
 } from "../ISpecificationRepository";
 
 class SpecificationRepository implements ISpecificationRepository {
+  private static INSTANCE: SpecificationRepository;
   private specifications: Specification[];
 
   constructor() {
     this.specifications = [];
   }
 
-  create({ name, description }: ICreateSpecificationDTO) {
+  public static getInstance(): SpecificationRepository {
+    if (!SpecificationRepository.INSTANCE) {
+      SpecificationRepository.INSTANCE = new SpecificationRepository();
+    }
+
+    return SpecificationRepository.INSTANCE;
+  }
+
+  create({ name, description }: ICreateSpecificationDTO): void {
     const specification = new Specification();
 
     Object.assign(specification, {
