b59f7cba55d672d4bf44b2f499e0257b1eae4d2a
Filipe <filipediascontato@gmail.com>
2021-07-31 15:10:59 -0300

    Refacts the specificationRepository name

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,23 +1,23 @@
 import { Specification } from "../../entities/Specification";
 import {
   ICreateSpecificationDTO,
-  ISpecificationRepository,
-} from "../ISpecificationRepository";
+  ISpecificationsRepository,
+} from "../ISpecificationsRepository";
 
-class SpecificationRepository implements ISpecificationRepository {
-  private static INSTANCE: SpecificationRepository;
+class SpecificationsRepository implements ISpecificationsRepository {
+  private static INSTANCE: SpecificationsRepository;
   private specifications: Specification[];
 
   constructor() {
     this.specifications = [];
   }
 
-  public static getInstance(): SpecificationRepository {
-    if (!SpecificationRepository.INSTANCE) {
-      SpecificationRepository.INSTANCE = new SpecificationRepository();
+  public static getInstance(): SpecificationsRepository {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
     }
 
-    return SpecificationRepository.INSTANCE;
+    return SpecificationsRepository.INSTANCE;
   }
 
   create({ name, description }: ICreateSpecificationDTO): void {
@@ -41,4 +41,4 @@ class SpecificationRepository implements ISpecificationRepository {
   }
 }
 
-export { SpecificationRepository };
+export { SpecificationsRepository };
