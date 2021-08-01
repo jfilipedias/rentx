fedd1dbc62dbab99043e7b2b624786ec8b23db38
Filipe <filipediascontato@gmail.com>
2021-08-01 19:15:06 -0300

    Implements the ensureAuthenticated middleware

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post("/", (request, response) => {
   return createSpecificationController.handle(request, response);
 });
