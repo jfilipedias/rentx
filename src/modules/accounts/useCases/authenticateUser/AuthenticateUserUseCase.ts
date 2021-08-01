35a01a53646f8d5ef9b8f7a203123661dfd0c2a4
Filipe <filipediascontato@gmail.com>
2021-08-01 18:30:24 -0300

    Implements authentication route

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,58 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("User email or password incorrect.");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("User email or password incorrect.");
+    }
+
+    const token = sign({}, "f34fa5ab9805c87d7a19050d14486e82", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    const session = {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+
+    return session;
+  }
+}
+
+export { AuthenticateUserUseCase };
