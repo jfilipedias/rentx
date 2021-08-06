ed2c8117ab9e0f63671521295d376c5a021205cd
Filipe <filipediascontato@gmail.com>
2021-08-05 22:40:59 -0300

    Implements avatar upload

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -16,12 +16,16 @@ class UsersRepository implements IUsersRepository {
     email,
     password,
     driver_license,
+    id,
+    avatar,
   }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
       email,
       password,
       driver_license,
+      id,
+      avatar,
     });
 
     await this.repository.save(user);
--- src/modules/accounts/useCases/uploadUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/uploadUserAvatar/UpdateUserAvatarController.ts
@@ -6,7 +6,7 @@ import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 class UpdateUserAvatarController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { id } = request.user;
-    const avatar_file = null;
+    const avatar_file = request.file.filename;
 
     const uploadUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
     await uploadUserAvatarUseCase.execute({ user_id: id, avatar_file });
