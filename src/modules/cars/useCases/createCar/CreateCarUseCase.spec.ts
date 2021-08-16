dc8b7ecd3e0f53a3b98e4428525797e35e417c1a
Filipe <filipediascontato@gmail.com>
2021-08-16 00:07:02 -0300

    Continues to implements the CreateCarUseCase

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,13 +1,25 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+
 import { CreateCarUseCase } from "./CreateCarUseCase";
 
 let createCarUseCase: CreateCarUseCase;
+let carsRepostitory: CarsRepositoryInMemory;
 
 describe("Create Car", () => {
   beforeEach(() => {
-    createCarUseCase = new CreateCarUseCase();
+    carsRepostitory = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepostitory);
   });
 
   it("Should be able to create a new car", async () => {
-    await createCarUseCase.execute();
+    await createCarUseCase.execute({
+      name: "Aventador",
+      description: "A sport car",
+      daily_rate: 1000,
+      license_plate: "NFA-5230",
+      fine_amount: 200,
+      brand: "Lamborghini",
+      category_id: "category",
+    });
   });
 });
