4dd2356c4715627c35cdb7d01eaabf3622d65369
Filipe <filipediascontato@gmail.com>
2021-08-22 23:23:55 -0300

    Ajust CreateCarUseCase for tests

--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,7 +1,9 @@
 import { inject, injectable } from "tsyringe";
 
 import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { AppError } from "@shared/errors/AppError";
 
 @injectable()
 class CreateCarUseCase {
@@ -18,8 +20,16 @@ class CreateCarUseCase {
     fine_amount,
     brand,
     category_id,
-  }: ICreateCarDTO): Promise<void> {
-    this.carsRepository.create({
+  }: ICreateCarDTO): Promise<Car> {
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(
+      license_plate
+    );
+
+    if (carAlreadyExists) {
+      throw new AppError("Car already exists!");
+    }
+
+    const car = await this.carsRepository.create({
       name,
       description,
       daily_rate,
@@ -28,6 +38,8 @@ class CreateCarUseCase {
       brand,
       category_id,
     });
+
+    return car;
   }
 }
 
