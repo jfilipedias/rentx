1b0043e498463cc04bf615e65ec9f3eb682bc0d2
Filipe <filipediascontato@gmail.com>
2021-08-22 23:23:26 -0300

    Implements the CreateCarUseCase tests

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 
 import { CreateCarUseCase } from "./CreateCarUseCase";
 
@@ -12,7 +13,7 @@ describe("Create Car", () => {
   });
 
   it("Should be able to create a new car", async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: "Aventador",
       description: "A sport car",
       daily_rate: 1000,
@@ -21,5 +22,45 @@ describe("Create Car", () => {
       brand: "Lamborghini",
       category_id: "category",
     });
+
+    expect(car).toHaveProperty("id");
+  });
+
+  it("Should not be able to create a car with an already registered license plate", async () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: "Car 1",
+        description: "A sport car",
+        daily_rate: 1000,
+        license_plate: "NFA-5230",
+        fine_amount: 200,
+        brand: "Lamborghini",
+        category_id: "category",
+      });
+
+      await createCarUseCase.execute({
+        name: "Car 2",
+        description: "A sport car",
+        daily_rate: 1000,
+        license_plate: "NFA-5230",
+        fine_amount: 200,
+        brand: "Lamborghini",
+        category_id: "category",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should be able to create a car with availability true by default", async () => {
+    const car = await createCarUseCase.execute({
+      name: "Aventador",
+      description: "A sport car",
+      daily_rate: 1000,
+      license_plate: "NFA-5230",
+      fine_amount: 200,
+      brand: "Lamborghini",
+      category_id: "category",
+    });
+
+    expect(car.available).toBe(true);
   });
 });
