530107ffe50d6c38689dfb59cba9b84abc6583bc
Filipe <filipediascontato@gmail.com>
2021-08-23 00:11:35 -0300

    Implements seed admin

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,13 +3,12 @@ import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import "@shared/container";
 import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
 
-import "@shared/infra/typeorm";
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
-
-import "@shared/container";
 
 const app = express();
 
---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,18 @@
+import { hash } from "bcryptjs";
+import { v4 as uuidv4 } from "uuid";
+
+import createConnection from "..";
+
+async function create() {
+  const connection = await createConnection("localhost");
+
+  const id = uuidv4();
+  const password = await hash("admin", 8);
+
+  await connection.query(
+    `INSERT INTO users(id, name, email, password, driver_license, is_admin, created_at) 
+    VALUES('${id}', 'Admin', 'admin@foobar.com', '${password}', '1234567890', true, 'now()')`
+  );
+}
+
+create().then(() => console.log("Admin user created!"));
