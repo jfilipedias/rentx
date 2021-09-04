af1c3031f47906ae787b46346e1fc6099b2c2bb1
Filipe <filipediascontato@gmail.com>
2021-09-04 20:16:05 -0300

    Creates the /cars/specifications/:id route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/createCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/listAvailabeCarsController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
@@ -9,6 +10,7 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const carsRoutes = Router();
 
 const createCarsController = new CreateCarController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.get("/available", listAvailableCarsController.handle);
@@ -17,6 +19,12 @@ carsRoutes.post(
   ensureAuthenticated,
   ensureAdmin,
   createCarsController.handle
+);
+carsRoutes.post(
+  "/specifications/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarSpecificationController.handle
 );
 
 export { carsRoutes };
