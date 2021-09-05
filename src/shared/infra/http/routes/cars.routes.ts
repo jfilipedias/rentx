e95ffc8f8bd4c139a0ed5dd9a7b284a7f695d6ba
Filipe <filipediascontato@gmail.com>
2021-09-05 18:03:44 -0300

    Creates the /cars/images/:id route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,30 +1,46 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/createCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/listAvailabeCarsController";
+import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImages/uploadCarImagesController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
+const uploadCarImages = multer(uploadConfig.upload("./tmp/cars"));
+
 const createCarsController = new CreateCarController();
 const createCarSpecificationController = new CreateCarSpecificationController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const uploadCarsImagesController = new UploadCarImagesController();
 
 carsRoutes.get("/available", listAvailableCarsController.handle);
+
 carsRoutes.post(
   "/",
   ensureAuthenticated,
   ensureAdmin,
   createCarsController.handle
 );
+
 carsRoutes.post(
   "/specifications/:id",
   ensureAuthenticated,
   ensureAdmin,
   createCarSpecificationController.handle
+);
+
+carsRoutes.post(
+  "/images/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  uploadCarImages.array("images"),
+  uploadCarsImagesController.handle
 );
 
 export { carsRoutes };
