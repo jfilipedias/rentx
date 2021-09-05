1c66b8a71325e109aae3ec28ae014456d6f91fd3
Filipe <filipediascontato@gmail.com>
2021-09-05 11:53:48 -0300

    Create CarImage Entity

---
+++ src/modules/cars/infra/typeorm/entities/CarImage.ts
@@ -0,0 +1,18 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+
+@Entity("cars_image")
+class CarImage {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  car_id: string;
+
+  @Column()
+  image_name: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
+
+export { CarImage };
