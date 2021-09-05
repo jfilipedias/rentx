8503de623c53254bb970ad6f98350d1c299d2e89
Filipe <filipediascontato@gmail.com>
2021-09-05 18:03:01 -0300

    Rename files

---
+++ src/modules/cars/useCases/uploadCarImages/uploadCarImagesController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UploadCarImagesUseCase } from "./uploadCarImagesUseCase";
+
+interface IFile {
+  filename: string;
+}
+
+class UploadCarImagesController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const images = request.files as IFile[];
+    const images_name = images.map((file) => file.filename);
+
+    const uploadCarImagesUseCase = container.resolve(UploadCarImagesUseCase);
+    await uploadCarImagesUseCase.execute({ car_id: id, images_name });
+
+    return response.status(201).send();
+  }
+}
+
+export { UploadCarImagesController };
