a464ce765385d0025e0f0a1d71a5d476c5c30b45
Filipe Dias <filipediascontato@gmail.com>
2021-09-22 22:23:58 -0300

    Fix CreateRentals migration

--- src/shared/infra/typeorm/migrations/1632063168849-CreateRentals.ts
+++ src/shared/infra/typeorm/migrations/1632063168849-CreateRentals.ts
@@ -10,11 +10,11 @@ export class CreateRentals1632063168849 implements MigrationInterface {
           { name: "car_id", type: "uuid" },
           { name: "user_id", type: "uuid" },
           { name: "start_date", type: "timestamp", default: "now()" },
-          { name: "end_date", type: "timestamp" },
+          { name: "end_date", type: "timestamp", isNullable: true },
           { name: "expected_return_date", type: "timestamp" },
-          { name: "total", type: "numeric" },
+          { name: "total", type: "numeric", isNullable: true },
           { name: "created_at", type: "timestamp", default: "now()" },
-          { name: "updated_at", type: "timestamp" },
+          { name: "updated_at", type: "timestamp", default: "now()" },
         ],
         foreignKeys: [
           {
