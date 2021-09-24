6bf3b18b5c52f75e180ba4820403e0a798d2d518
filipe <filipedias@alphasistemas.com.br>
2021-09-24 18:10:42 -0300

    Change AAuthenticateUserUseCase status code error

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -24,19 +24,19 @@ class AuthenticateUserUseCase {
   constructor(
     @inject("UsersRepository")
     private usersRepository: IUsersRepository
-  ) {}
+  ) { }
 
   async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new AppError("User email or password incorrect.");
+      throw new AppError("User email or password incorrect.", 401);
     }
 
     const passwordMatch = await compare(password, user.password);
 
     if (!passwordMatch) {
-      throw new AppError("User email or password incorrect.");
+      throw new AppError("User email or password incorrect.", 401);
     }
 
     const token = sign({}, "f34fa5ab9805c87d7a19050d14486e82", {
