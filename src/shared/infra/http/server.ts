096f26a1677b8eda3bb6399c291e8dfa7a5f7815
jfilipedias <filipediascontato@gmail.com>
2021-10-10 19:31:31 -0300

    Fix project settings to POSIX

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,6 +3,4 @@ import { app } from "./app";
 const host = process.env.HOST || "http://localhost";
 const port = process.env.PORT || "3333";
 
-app.listen(port, () =>
-  console.log(`Server is running in ${host}:${port}...`)
-);
+app.listen(port, () => console.log(`Server is running in ${host}:${port}...`));
