d70d5414fca5ad1f4814b81445c0b61b5c408e61
jfilipedias <filipediascontato@gmail.com>
2021-10-16 19:49:33 -0300

    Fix parameters send to devolutionRentalUseCase

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalController.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalController.ts
@@ -5,11 +5,12 @@ import { DevolutionRentalUseCase } from "./DevolutionRentalUseCase";
 
 class DevolutionRentalController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const { id: user_id } = request.user;
     const { id } = request.route;
 
     const devolutionRentalUseCase = container.resolve(DevolutionRentalUseCase);
 
-    const rental = await devolutionRentalUseCase.execute(id);
+    const rental = await devolutionRentalUseCase.execute({ id, user_id });
 
     return response.status(200).json(rental);
   }
