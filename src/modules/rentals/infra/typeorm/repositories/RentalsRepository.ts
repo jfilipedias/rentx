aaa8073afadc2863134ee7fdacc2cf68af24b584
jfilipedias <filipediascontato@gmail.com>
2021-10-16 19:43:43 -0300

    Create the findById methods in the rentals repositories

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -16,25 +16,44 @@ class RentalsRepository implements IRentalsRepository {
     car_id,
     user_id,
     expected_return_date,
+    id,
+    end_date,
+    total,
   }: ICreateRentalDTO): Promise<Rental> {
+    console.log("Start to create the rental...");
     const rental = this.repository.create({
       car_id,
       user_id,
       expected_return_date,
+      id,
+      end_date,
+      total,
     });
+
+    console.log("Create the rental...");
 
     await this.repository.save(rental);
 
+    console.log("Save the rental...");
+    return rental;
+  }
+
+  async findById(id: string): Promise<Rental> {
+    const rental = await this.repository.findOne(id);
     return rental;
   }
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    const rental = await this.repository.findOne({ car_id });
+    const rental = await this.repository.findOne({
+      where: { car_id, end_date: null },
+    });
     return rental;
   }
 
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    const rental = await this.repository.findOne({ user_id });
+    const rental = await this.repository.findOne({
+      where: { user_id, end_date: null },
+    });
     return rental;
   }
 }
--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -3,6 +3,7 @@ import { Rental } from "../infra/typeorm/entities/Rental";
 
 interface IRentalsRepository {
   create(data: ICreateRentalDTO): Promise<Rental>;
+  findById(id: string): Promise<Rental>;
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
 }
