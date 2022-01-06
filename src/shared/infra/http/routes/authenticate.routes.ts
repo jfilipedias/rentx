fc1e2e6ca17e0a49d09b3cc5ec4bf3fc8e015190
Filipe <filipediascontato@gmail.com>
2022-01-06 13:48:09 -0300

    Renames use case folder

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController";
-import { RefreshTokenController } from "@modules/accounts/useCases/RefreshToken/RefreshTokenController";
+import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 
 const authenticateRoutes = Router();
 
