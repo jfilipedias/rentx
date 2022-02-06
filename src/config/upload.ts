a03f6853b0404254161f86e7f8eefe9bc245a531
Filipe <filipediascontato@gmail.com>
2022-02-06 20:57:33 -0300

    Fixes upload config

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,12 @@ import crypto from "crypto";
 import multer from "multer";
 import { resolve } from "path";
 
+const tmpFolder = resolve(__dirname, "..", "..", "tmp");
 export default {
-  tmpFolder: resolve(__dirname, "..", "..", "tmp"),
+  tmpFolder,
+
   storage: multer.diskStorage({
+    destination: tmpFolder,
     filename: (request, file, callback) => {
       const fileHash = crypto.randomBytes(16).toString("hex");
       const filename = `${fileHash}-${file.originalname}`;
