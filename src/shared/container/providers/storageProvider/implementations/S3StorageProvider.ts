f5aa60597e3958c500694bf37e1e7377ff05b113
Filipe <filipediascontato@gmail.com>
2022-02-07 20:51:35 -0300

    Implements the S3StorageProvider

--- src/shared/container/providers/storageProvider/implementations/S3StorageProvider.ts
+++ src/shared/container/providers/storageProvider/implementations/S3StorageProvider.ts
@@ -1,11 +1,49 @@
+import { S3 } from "aws-sdk";
+import fs from "fs";
+import mime from "mime";
+import { resolve } from "path";
+
+import upload from "@config/upload";
+
 import { IStorageProvider } from "../IStorageProvider";
 
 class S3StorageProvider implements IStorageProvider {
-  save(file: string, folder: string): Promise<string> {
-    throw new Error("Method not implemented.");
+  private client: S3;
+
+  constructor() {
+    this.client = new S3({
+      region: process.env.AWS_BUCKET_REGION,
+    });
   }
-  delete(file: string, folder: string): Promise<void> {
-    throw new Error("Method not implemented.");
+
+  async save(file: string, folder: string): Promise<string> {
+    const originalName = resolve(upload.tmpFolder, file);
+
+    const fileContent = await fs.promises.readFile(originalName);
+    const contentType = mime.getType(originalName);
+
+    await this.client
+      .putObject({
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+        Key: file,
+        ACL: "public-read",
+        Body: fileContent,
+        ContentType: contentType,
+      })
+      .promise();
+
+    await fs.promises.unlink(originalName);
+
+    return file;
+  }
+
+  async delete(file: string, folder: string): Promise<void> {
+    await this.client
+      .deleteObject({
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+        Key: file,
+      })
+      .promise();
   }
 }
 
