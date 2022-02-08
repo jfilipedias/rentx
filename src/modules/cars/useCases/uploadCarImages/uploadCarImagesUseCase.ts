bd61ad982017ce44cfe3986de30e4def7730b4c7
Filipe <filipediascontato@gmail.com>
2022-02-07 21:25:59 -0300

    Implements car image upload

--- src/modules/cars/useCases/uploadCarImages/uploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/uploadCarImagesUseCase.ts
@@ -1,6 +1,9 @@
 import { inject, injectable } from "tsyringe";
 
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { IStorageProvider } from "@shared/container/providers/storageProvider/IStorageProvider";
+import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
   car_id: string;
@@ -10,13 +13,24 @@ interface IRequest {
 @injectable()
 class UploadCarImagesUseCase {
   constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository,
     @inject("CarsImagesRepository")
-    private carsImagesRepository: ICarsImagesRepository
+    private carsImagesRepository: ICarsImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
+    const car = await this.carsRepository.findById(car_id);
+
+    if (!car) {
+      throw new AppError("Car not found!", 404);
+    }
+
     images_name.map(async (image_name) => {
       await this.carsImagesRepository.create(car_id, image_name);
+      await this.storageProvider.save(image_name, "cars");
     });
   }
 }
