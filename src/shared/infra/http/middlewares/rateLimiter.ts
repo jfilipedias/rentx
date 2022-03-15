b89fbe2df19e4957db9b7f4f1d0028016176fb66
Filipe Dias <filipediascontato@gmail.com>
2022-03-14 23:05:05 -0300

    Fixes rateLimiter middleware

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -4,27 +4,29 @@ import { createClient } from "redis";
 
 import { AppError } from "@shared/errors/AppError";
 
-const redisClient = createClient({
-  legacyMode: true,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: Number(process.env.REDIS_PORT),
-    sessionTimeout: 20,
-  },
-});
-
-const redisLimiter = new RateLimiterRedis({
-  storeClient: redisClient,
-  keyPrefix: "rateLimiter",
-  points: 10,
-  duration: 1,
-});
-
 export default async function rateLimiter(
   request: Request,
   response: Response,
   next: NextFunction
 ): Promise<void> {
+  const redisClient = createClient({
+    legacyMode: true,
+    socket: {
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+      sessionTimeout: 20,
+    },
+  });
+
+  await redisClient.connect();
+
+  const redisLimiter = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: "rateLimiter",
+    points: 10,
+    duration: 1,
+  });
+
   try {
     await redisLimiter.consume(request.ip);
     return next();
